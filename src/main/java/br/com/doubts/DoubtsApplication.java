02f7c408bfbe186860c984ac61ec846feaec9b7d
Nailton2002 <jnmp25jnmp@gmail.com>
2022-09-22 15:39:35 -0300

    Projeto criado

---
+++ src/main/java/br/com/doubts/DoubtsApplication.java
@@ -0,0 +1,13 @@
+package br.com.doubts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/doubts/DoubtsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doubts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
